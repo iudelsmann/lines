aa981fbdfb62f767568eaaece1c7a4afcb833e36
Ivo Udelsmann <ivo.udelsmann@eldorado.org.br>
2017-08-08 16:10:15 -0300

    Adds a simple api endpoint as example

--- server/main.js
+++ server/main.js
@@ -6,3 +6,12 @@ import '../imports/api/lines/collection';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+// Example of an API route to showcase we can have an api
+JsonRoutes.add("get", "/api/bla", function (req, res, next) {
+  JsonRoutes.sendResult(res, {
+    data: {
+      bla: 'bla',
+    },
+  });
+});
